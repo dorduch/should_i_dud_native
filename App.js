0fce0664a9520de7c6ec5778389764864fd32764
Dor Duchovni <dor@falcore.co>
2017-07-09 12:57:48 +0300

    refresh!

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import React, {Component} from 'react';
 import moment from 'moment';
 import {getUvIndex, getUvIndexLevel, levels} from './services/weather';
 import {getAddress} from './services/location';
+import {StackNavigator} from 'react-navigation';
+import {Constants} from 'expo';
+
 import {
   ScrollView,
   View,
   ActivityIndicator,
   RefreshControl,
+  StyleSheet,
 } from 'react-native';
 import {Text, Header} from 'react-native-elements';
 
-export default class App extends Component {
+class MainView extends Component {
+  static navigationOptions = {
+    title: 'Should i Dud?',
+  };
+
   state = {
     uvIndex: 0,
     loading: true,
@@ -51,29 +59,51 @@ export default class App extends Component {
       <View
         style={{...styles.container, backgroundColor: this.state.level.color}}
       >
-        <Header
-          statusBarProps={{barStyle: 'light-content'}}
-          centerComponent={{
-            text: 'Should i Dud?',
-            style: {color: 'white', fontSize: 20},
-          }}
-          backgroundColor={'black'}
-        />
         {this.state.loading
           ? <ActivityIndicator color="white" />
           : <View style={styles.container}>
-              <Text style={{fontSize: 60, marginBottom: 10}}>
+              <View
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                }}
+              >
+                <ScrollView
+                  style={{display: 'flex'}}
+                  contentContainerStyle={{flexGrow: 1}}
+                  refreshControl={
+                    <RefreshControl
+                      refreshing={this.state.loading}
+                      onRefresh={this._onRefresh.bind (this)}
+                    />
+                  }
+                />
+              </View>
+              <Text style={{fontSize: 60, marginBottom: 10, color: 'white'}}>
                 {this.state.level.label}
               </Text>
-              <Text style={{fontSize: 25, marginBottom: 10}}>
+              <Text style={{fontSize: 25, marginBottom: 10, color: 'white'}}>
                 {this.state.level.subtitle}
               </Text>
-              <Text style={{fontSize: 15, marginBottom: 10}}>
+              <Text style={{fontSize: 15, marginBottom: 10, color: 'white'}}>
                 {this.state.address}
               </Text>
             </View>}
       </View>
     );
+  }
+}
+
+const MainScreenNavigator = StackNavigator ({
+  Home: {screen: MainView},
+});
+
+export default class App extends Component {
+  render () {
+    return <MainScreenNavigator />;
   }
 }
 
